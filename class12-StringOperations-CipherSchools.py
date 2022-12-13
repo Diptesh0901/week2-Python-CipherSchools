c9b77e40eacafd1b959d1b03c627303481283355
Diptesh0901 <119107331+Diptesh0901@users.noreply.github.com>
2022-12-13 22:11:05 +0530

    Add files via upload

---
+++ class12-StringOperations-CipherSchools.py
@@ -0,0 +1,28 @@
+# some string operations
+
+a = 5
+print("value of a is %d" % (a))
+print("value of a is {}".format(a))
+
+a, b, c, d = 1, 2, 3, 4
+print("a = {2}, b = {1}, c = {0}".format(c, b, a))
+
+name = "Jatin katyal"
+print(name)
+company = "Shuttle"
+print(company)
+
+print(f"name = {name}")
+
+print("Hello {name} welcome to my facebook".format(name = name, comapny = company))
+
+print(f"name = {name}")
+print(len(r"a\nb"))
+
+print("1, 2, 3, 4, 5".split(","))
+
+print("jatin katyal".replace("a", "z"))
+print("jatin katyal".count("a"))
+
+
+
